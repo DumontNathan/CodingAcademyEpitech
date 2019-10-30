6871572d4c2ba85c41a271f9f3969550c24a5d40
DUMONT Nathan <dnathan@localhost.localdomain>
2019-10-30 17:44:53 +0100

    sapin

--- tree.c
+++ tree.c
@@ -10,41 +10,41 @@ int error_messages(int etages);
 
 void tree(int etages)
 {
-    error_messages(etages);
+    error_messages(etages); // Affiche les messages d'erreur
 
     int axe = get_axe(etages);
-    int *ptraxe = &axe;
-    for (int i = 1; i <= etages; i++)
+    int *ptraxe = &axe;                  // Garde la valeur de l'axe en fonction du nombre d'étages (Base du dernier etage / 2)
+    for (int i = 1; i <= etages; i++)   // Dessine chaque etage
     {
         draw_last_triangle(i);
         print_tree(i, ptraxe);
     }
-    print_trunk(etages, ptraxe);
+    print_trunk(etages, ptraxe);  // Dessine le tronc
 }
 
 int *draw_last_triangle(int etages)
 {
-    int *init = malloc(4*sizeof(int));
-    int debut = 1;
-    int lignes = 4; 
-    int base = 7;
-    int compteur = 4;
+    int *init = malloc(4*sizeof(int)); // Tableau que la fonction return
+    int debut = 1;                     // Nb d'* de la première ligne d'un étage
+    int lignes = 4;                    // Nombre de lignes de l'étage
+    int base = 7;                      // Nb d'* de la derniere ligne d'un étage
+    int compteur = 4;                  // Nb d'* à rajouter à chaque première ligne (début)
 
     if (etages > 1)
     {
-        for (int i = 1; i < etages; i++)  // pour chaque etage
+        for (int i = 1; i < etages; i++)  // Pour chaque etage
         {
-            if ( (i%2) == 0)  // si létage est paire, début = début + 2
+            if ( (i%2) == 0)  // Si l'étage est paire, le nb d'* a rajouter sur la première ligne augmente (début) 
             {
-                compteur += 2; // compteur pour augmenter le nb d'étoile du debut a chaque nombre impaire
+                compteur += 2; 
             }
-            debut = debut + compteur;
-            lignes++;
-            base = debut + ((lignes-1) * 2);  //le nombre d'étoiles de la base
+            debut = debut + compteur;  // Incrémente le nb d'* a rajouter sur la première ligne (début)
+            lignes++;                  // Incrémente le nb de lignes (+1 ligne par étage)
+            base = debut + ((lignes-1) * 2);  //Le nombre d'* de la base
         }
     }
     
-    init[0] = debut;
+    init[0] = debut;    // Place les valeurs de chaque variable dans le tableau à return
     init[1] = lignes;
     init[2] = base;
     init[3] = compteur;
@@ -53,21 +53,21 @@ int *draw_last_triangle(int etages)
 }
 
 
-void print_tree(int etages, int* ptraxe)
+void print_tree(int etages, int* ptraxe)  // affiche le sapin
 {
     int *init = draw_last_triangle(etages); 
-    int debut = init[0];
+    int debut = init[0];  // Recupération des valeurs
     int lignes = init[1];  
 
-    for (int i = 0; i < lignes; i++)
+    for (int i = 0; i < lignes; i++)  // Pour chaque lignes
        {
-           for (int k = 0; k < *ptraxe - (debut/2) + 1; k++) // ptraxe = axe central (base/2) - nb d'étoiles (debut)/2
+           for (int k = 0; k < *ptraxe - (debut/2) ; k++) // ptraxe = axe central (base/2) - nb d'étoiles (debut)/2
             {
-                my_putchar(' ');
+                my_putchar(' ');    //Mettre autant d'espace que la position de l'axe - la taille de la ligne/2
             }
             
 
-           for (int j = 0; j < debut; j++)  // afficher une etoile autant que la taille du debut
+           for (int j = 0; j < debut; j++)  // afficher autant d'* que la taille de la ligne (-1 à chaque tour)
            {
                my_putchar('*');
            }
@@ -76,7 +76,7 @@ void print_tree(int etages, int* ptraxe)
        }
 }
 
-int get_axe(int etages)
+int get_axe(int etages)  // récupère la taille de la base du dernier etage et la divise par 2 pour obtenir l'axe central (position de la premiere etoile)
 {
     int *init = draw_last_triangle(etages); 
     int base = init[2];
@@ -85,10 +85,10 @@ int get_axe(int etages)
     return axe;
 }
 
-void print_trunk(int etages, int *ptraxe)
+void print_trunk(int etages, int *ptraxe)  // affiche le tronc
 {
     int *init = draw_last_triangle(etages); 
-    int compteur = 1;
+    int compteur = 1; // Représente le nb d'| (qui augmente de 2 a chaque etage impaire)
 
     for (int i = 1; i <= etages; i++) // en fonction du nombre d'étages
     {
@@ -98,15 +98,15 @@ void print_trunk(int etages, int *ptraxe)
         }
     }
     
-    for (int i = 1; i <= etages; i++)
+    for (int i = 1; i <= etages; i++) // une fois qu'on a le bon nombre d'| à afficher (compteur)
     {
 
-        for (int j = 0; j < (*ptraxe +1) - compteur/2; j++) // afficher autant d'espaces que la moitié de l'axe 
+        for (int j = 0; j < (*ptraxe) - compteur/2; j++) // afficher autant d'espaces que la moitié de l'axe - le nb d'| qui augmente /2
         {
             my_putchar(' ');
         }   
 
-        for (int l = 0; l < compteur; l++) 
+        for (int l = 0; l < compteur; l++)   // affiche autant d'etages de | que d'étages de sapin
         {
             my_putchar('|');
         }
